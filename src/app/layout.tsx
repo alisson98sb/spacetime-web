157834b82af344501cc2fedffa94a40e9c18b165
alisson98sb <alisson98sb@gmail.com>
2023-05-19 14:49:08 -0300

    Finished home layout, hero, signin, copyright, configuração da fonte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,15 @@ import { ReactNode } from 'react'
 import './globals.css'
 import {
   Roboto_Flex as Roboto,
-  Bai_Jamjuree as Jamjuree,
+  Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
 
-const baiJamjuree = Jamjuree({
+const baiJamjuree = BaiJamjuree({
   subsets: ['latin'],
   weight: '700',
   variable: '--font-bai-jamjuree',
 })
-const roboto = Roboto({ subsets: ['latin'], variable: '--fonts-roboto' })
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 
 export const metadata = {
   title: 'NLW Spacetime',
